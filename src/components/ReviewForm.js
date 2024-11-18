ae64271702a28c0f30b14bca35efe95d1d7c3ca2
chanoh6 <cocosh6@gmail.com>
2024-11-18 21:26:01 +0900

    feat: img 파일 input 추가

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,35 +1,51 @@
 import { useState } from "react";
+import FileInput from "./FileInput";
 
 const ReviewForm = () => {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+    imgFile: null,
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
 
-  const handleRatingChange = (e) => {
-    // 입력 값이 숫자가 아닌 경우 처리
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
   };
 
   const handleSubmit = (e) => {
     // 기본 동작 제한
     e.preventDefault();
-    console.log(title, rating, content);
+    console.log(values.title, values.rating, values.content);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+      <FileInput
+        name="imgFile"
+        value={values.imgFile}
+        onChange={handleChange}
+      />
+      <input name="title" value={values.title} onChange={handleInputChange} />
+      <input
+        type="number"
+        name="rating"
+        value={values.rating}
+        onChange={handleInputChange}
+      />
+      <textarea
+        name="content"
+        value={values.content}
+        onChange={handleInputChange}
+      />
       <button type="submit">확인</button>
     </form>
   );
