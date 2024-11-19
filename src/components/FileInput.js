f4339d5027a832dd365c8f01d9243f8e7a57b502
chanoh6 <cocosh6@gmail.com>
2024-11-19 21:24:32 +0900

    feat: 이미지 미리보기 기능 추가

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const FileInput = ({ name, value, onChange }) => {
   // 실제 DOM 노드를 직접 참조 가능
   // DOM 노드는 렌더링이 끝나야 생겨서 Ref 객체의 current 값도 렌더링이 끝나야 생김
   const inputRef = useRef();
+  const [preview, setPreview] = useState();
 
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
@@ -19,9 +20,31 @@ const FileInput = ({ name, value, onChange }) => {
     onChange(name, null);
   };
 
+  useEffect(() => {
+    if (!value) return;
+
+    // Object URL 생성 (문자열 return)
+    // Object URL을 만들면 웹 브라우저는 메모리를 할당하고 파일에 해당하는 주소를 만들어준다.
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    // 사이드이펙트 정리
+    return () => {
+      setPreview();
+      // revokeObjectURL : 생성한 ObjdectURL 해제
+      URL.revokeObjectURL(nextPreview);
+    };
+  }, [value]);
+
   return (
     <div>
-      <input type="file" onChange={handleChange} ref={inputRef} />
+      <img src={preview} alt="이미지 미리보기" />
+      <input
+        type="file"
+        accept="image/png, image/jpeg"
+        onChange={handleChange}
+        ref={inputRef}
+      />
       {value && <button onClick={handleClearClick}>X</button>}
     </div>
   );
