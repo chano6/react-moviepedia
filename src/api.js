89e77d2abddf944871e56a8eb17adf9d8ee23eeb
chanoh6 <cocosh6@gmail.com>
2024-11-21 20:34:45 +0900

    feat: 리뷰 작성 기능 추가

--- src/api.js
+++ src/api.js
@@ -23,7 +23,7 @@ export const createReview = async (formData) => {
   });
 
   if (!response.ok) {
-    throw new Error("리뷰를 불러오는데 실패했습니다.");
+    throw new Error("리뷰를 생성하는데 실패했습니다.");
   }
 
   const body = await response.json();
